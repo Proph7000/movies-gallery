8d6a9e0c4f0d0c706a9fa303c868fed135aae064
Andrii Andrieiev <andrii.andrieiev@arbitas.com>
2024-06-30 13:45:05 +0300

    fixes

--- src/entities/movie/ui/genre-item.tsx
+++ src/entities/movie/ui/genre-item.tsx
@@ -7,7 +7,7 @@ interface IProps {
 export function GenreItem({ genre }: IProps) {
   return (
     <Paper sx={{ p: '4px 8px', borderRadius: 1 }}>
-      <Typography variant="body4" display="block" textAlign="center">
+      <Typography variant="body2" display="block" textAlign="center">
         {genre}
       </Typography>
     </Paper>
